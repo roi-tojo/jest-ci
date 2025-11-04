b3de6e9780e4639a6f806299c7b2003f6e1f81a3
fancrew-tojo <tojo@fancrew.co.jp>
2025-11-04 14:05:26 +0900

    NestJS, Jest, サンプル実装とテスト追加

---
+++ src/hello.controller.spec.ts
@@ -0,0 +1,18 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { HelloController } from './hello.controller';
+
+describe('HelloController', () => {
+  let helloController: HelloController;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [HelloController],
+    }).compile();
+
+    helloController = module.get<HelloController>(HelloController);
+  });
+
+  it('should return "Hello World!"', () => {
+    expect(helloController.getHello()).toBe('Hello World!');
+  });
+});
